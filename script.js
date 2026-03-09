333e26d5c3a6f2a1638592ea1db8618c6c9e717c
prakhersarawat216-hub <prakhersaraswat216@gmail.com>
2026-03-09 10:22:38 +0530

    all files

---
+++ script.js
@@ -0,0 +1,12 @@
+// const sym1 = Symbol(4)
+// console.log(sym1);
+// const sym2 = Symbol(4)
+// console.log(sym2);
+// if(sym1 === sym2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+var a=100; var b=10; var c=23; var linebreak = "<br>";
+document.write
